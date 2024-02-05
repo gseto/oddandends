7d078747c2b620122410b765a295cab667e3ecc1
gseto <garrisonseto@gmail.com>
2024-02-05 13:12:58 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const nav = document.querySelector('.navbar');
 const collaspebar = document.querySelector('#navbarCollapse');
+const hamburger = document.querySelector('.navbar-toggler');
 var boo = 1;
 window.addEventListener('scroll', () => {
   if(window.scrollY >= 56) {
@@ -15,7 +16,19 @@ collaspebar.addEventListener('hidden.bs.collapse', function () {
     boo = 1;
   }
 });
-collaspebar.addEventListener('shown.bs.collapse', function () {
+// collaspebar.addEventListener('shown.bs.collapse', function () {
+//   nav.classList.add('navbar-scrolled')
+//   boo = 0;
+// });
+
+
+hamburger.addEventListener('click', function () {
+  if(window.scrollY < 56) {
+    nav.classList.remove('navbar-scrolled')
+    boo = 1;
+  }
+});
+hamburger.addEventListener('click', function () {
   nav.classList.add('navbar-scrolled')
   boo = 0;
 });
